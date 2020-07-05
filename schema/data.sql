6c5b0550c497af6f978f229a2e4ade212fc5ffc4
Neeraj Sharma <neerajraja2001@yahoo.co.in>
2020-07-05 21:57:42 +0200

    Adding schema and sample data

---
+++ schema/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO RECIPE (recipe_id, creation_date,suitable_for,veg) VALUES(1,'2020-07-03 07:07:18.428',5,1);
+INSERT INTO ingredients(ing_id,recipe_id, item) VALUES(1,1,"Turmaric");
+INSERT INTO ingredients(ing_id,recipe_id, item) VALUES(2,1,"Red Chiili");
+
+
+
+
